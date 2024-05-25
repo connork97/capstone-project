335222090b259d5c79256fc74c7bff3350ceca06
Connor Kormos <connorkormos97@gmail.com>
2024-05-25 16:20:36 -0700

    changed navbar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
             else setLoadingText("You're ready to go!")
         }
     }, [loadingText, currentUser, allEvents, allMemberships, allSessions])
-    // console.log(currentUser.length)
+
     return (
         <div className="mainDiv">
             <br></br><br></br><br></br><br></br><br></br>
@@ -32,6 +32,8 @@ const Home = () => {
             {loadingText !== "You're ready to go!" && <h2 className={styles.loadedH2}>Please allow the web service to spin up.</h2>}   
             {loadingText !== "You're ready to go!" && <h2 className={styles.loadedH2}>This may take up to one minute.</h2>}
             <h2 className={loadingText === "You're ready to go!" ? styles.loadedH2 : styles.loadingH2}>{loadingText}</h2>
+            {/* <img src='https://media.discordapp.net/attachments/1036342122191781889/1243375755778457711/korntoast_A_fit_woman_who_is_an_employee_of_a_rock_climbing_gym_87c69515-9755-4906-a917-1ecc840d82a7.png?ex=66513f68&is=664fede8&hm=5be5315745cbe6fcfc14f77a9c87b6c9b434599f03decf91a490810d04d36776&=&format=webp&quality=lossless&width=820&height=546'></img> */}
+            {/* <img src='https://media.discordapp.net/attachments/1036342122191781889/1243377073230319637/korntoast_A_man_at_home_on_his_couch_happily_typing_on_his_lapt_fa700e4e-0390-4eef-8119-6e8ba1697bda.png?ex=665140a3&is=664fef23&hm=938c846b13a9dd2e05d788aec7c23025399e05e8af4ea6bdb5b585de40e26d92&=&format=webp&quality=lossless&width=820&height=546'></img> */}
         </div>
     )
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import styles from './NavBar.module.css'
+import peakSyncLogo from '../images/PeakSync.png';
 
 import { Link } from "react-router-dom"
 import { useContext } from 'react'
@@ -8,13 +9,16 @@ import Dropdown from 'react-bootstrap/Dropdown'
 
 const NavBar = () => {
 
-    const { currentUser } = useContext(LoggedInUserContext)
+    const { currentUser } = useContext(LoggedInUserContext);
       
     return (
         <div className={styles.navBarDiv}>
-            <Link to="/" exact="true" className="navBarLink">Home</Link>
-            <Link to="/calendar" exact="true" className="navBarLink">Calendar</Link>
-            <Dropdown className="navBarLink">
+            <Link to="/" exact="true" className={styles.navBarLink}><img className={styles.navBarLink} src={peakSyncLogo}></img></Link>
+            {/* <Link to="/" exact="true" className="navBarLink">Home</Link> */}
+            <div className={styles.navWrap}>
+
+            <Link to="/calendar" exact="true" className={styles.navBarLink}>Calendar</Link>
+            <Dropdown className={styles.navBarLink}>
                 <Dropdown.Toggle className={styles.dropdownToggle}>Rates and Offerings</Dropdown.Toggle>
                 <Dropdown.Menu className={styles.dropdownMenu}>
                     <Dropdown.Item>
@@ -25,8 +29,9 @@ const NavBar = () => {
                     </Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
-            <Link to="/account" exact="true" className="navBarLink">Account</Link>
-            {currentUser.admin && <Link to="/admin-dashboard" exact="true" className="navBarLink">Dashboard</Link>}
+            <Link to="/account" exact="true" className={styles.navBarLinkActive}>Account</Link>
+            {currentUser.admin && <Link to="/admin-dashboard" exact="true" className={styles.navBarLinkActive}>Dashboard</Link>}
+            </div>
         </div>
     )
 }
